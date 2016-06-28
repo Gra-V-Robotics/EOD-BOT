e7decc52c875052c01552a2b22e4b930aa58ec73
Gra-V-Robotics <comcelion@gmail.com>
2016-06-28 18:00:24 -0400

    Home Commit - Adam
    Changed a few functions and removed comments TODO: Integrate CAN.

--- EODBOT/src/org/usfirst/frc/team5816/robot/OI.java
+++ EODBOT/src/org/usfirst/frc/team5816/robot/OI.java
@@ -1,37 +1,24 @@
 package org.usfirst.frc.team5816.robot;
 
-//Imports required classes.
 import edu.wpi.first.wpilibj.Joystick;
 
-//Beginning of class file.
 public class OI {
-	
-	//Defines the joysticks as Joysticks.
+
 	static Joystick leftJoystick, rightJoystick, XboxController;
-	//Creates variables for buttons, used later.
 	static boolean lowGearButton, highGearButton;
-	//Creates variables for the ports the Joysticks plug in to.
 	static int leftJoystickPort = 0;
 	static int rightJoystickPort = 1;
 	static int XboxControllerPort = 2;
-	
-	//Creates the function joystickInit().
+
 	public void joystickInit() {
-		
-		//Initializes the Joysticks to their ports.
+
 		leftJoystick = new Joystick(leftJoystickPort);
 		rightJoystick = new Joystick(rightJoystickPort);
 		XboxController = new Joystick(XboxControllerPort);
 		
 	}
-	
-	//Creates the function joystickButtons().
+
 	public void joystickButtons() {
-		
-		//Sets the variables created above to actual buttons on the joysticks.
-		//lowSpeedButton = leftJoystick.getRawButton(1);
-		//mediumSpeedButton = leftJoystick.getRawButton(2);
-		//highSpeedButton = leftJoystick.getRawButton(3);
 		
 		lowGearButton = leftJoystick.getRawButton(1);
 		highGearButton = rightJoystick.getRawButton(1);
--- EODBOT/src/org/usfirst/frc/team5816/robot/UtilityPrograms.java
+++ EODBOT/src/org/usfirst/frc/team5816/robot/UtilityPrograms.java
@@ -1,74 +1,51 @@
 package org.usfirst.frc.team5816.robot;
 
-//Imports the required classes.
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-//Creates the class UtilityPrograms
 public class UtilityPrograms {
-	
-	//Creates the names for the different programs.
+
 	static final String defaultTestProgram = "Default Test Program";
 	static final String secondaryTestProgram = "Secondary Test Program";
 	static final String tertiaryTestProgram = "Tertiary Test Program";
-	//Creates the name for the choosing variable.
 	static String utilityProgramSelected;
-	//Creates the SendableChooser.
 	static SendableChooser chooser;
-	
-	//Creates the function UtilityProgramsButtons().
+
 	public static void UtilityProgramsButtons() {
-		
-		//Initializes the SendableChooser.
+
 		chooser = new SendableChooser();
-		//Adds the default program to the chooser.
 		chooser.addDefault("Default Test Program", defaultTestProgram);
-		//Adds the other programs to the chooser.
 		chooser.addObject("Secondary Test Program", secondaryTestProgram);
 		chooser.addObject("Tertiary Test Program", tertiaryTestProgram);
-		//Places the chooser on the dashboard.
 		SmartDashboard.putData("Utility Program Choices", chooser);
 		
 	}
-	
-	//Creates the function UtilityProgramsControl().
+
 	public static void UtilityProgramsControl() {
-		
-		//Creates a switch case for the program selection.
+
 		switch(utilityProgramSelected) {
-		//Creates the secondaryTestProgram.
 		case secondaryTestProgram:
-			//Drives the robot forward.
 			RobotMap.drive1.drive(1.0, 0.0);
 			RobotMap.drive2.drive(1.0, 0.0);
 			RobotMap.drive3.drive(1.0, 0.0);
 			break;
-		//Creates the tertiaryTestProgram.
 		case tertiaryTestProgram:
-			//Extends the actuator.
 			RobotMap.solenoid.set(true);
-			//Delays the program for 5 seconds.
 			Timer.delay(5);
-			//Retracts the actuator.
 			RobotMap.solenoid.set(false);
 			break;
-		//Creates the defaultTestProgram
 		case defaultTestProgram:
-			//Drives the robot backward.
 			RobotMap.drive1.drive(-1.0, 0.0);
 			RobotMap.drive2.drive(-1.0, 0.0);
 			RobotMap.drive3.drive(-1.0, 0.0);
 		}
 		
 	}
-	
-	//Creates the function UtilityProgramsInit().
+
 	public static void UtilityProgramsInit() {
-		
-		//Gets the chosen program.
+
 		utilityProgramSelected = (String) chooser.getSelected();
-		//Displays the chosen program.
 		System.out.println("Utility Program Selected: " + utilityProgramSelected);
 		
 	}
