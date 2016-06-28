3f3f234cf2136b1ee3a85218b7c7e31fe7082372
Gra-V-Robotics <comcelion@gmail.com>
2016-06-27 20:18:35 -0400

    Home Commit.
    Added comments and changed the name of a function so it wouldn't use a
    constructor name.

--- EODBOT/src/org/usfirst/frc/team5816/robot/Robot.java
+++ EODBOT/src/org/usfirst/frc/team5816/robot/Robot.java
@@ -1,49 +1,68 @@
 package org.usfirst.frc.team5816.robot;
 
+//Imports required classes.
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+//Creates the Robot class and extends it to the functions of the IterativeRobot class.
 public class Robot extends IterativeRobot {
 
+	//Creates the function robotInit().
     public void robotInit() {
     	
+    	//Runs the function UtilityProgramsButtons() in the UtilityPrograms class.
     	UtilityPrograms.UtilityProgramsButtons();
+    	//Runs the function Camera_Config() in the RobotMap class.
     	RobotMap.Camera_Config();
 
     }
 
+    //Creates the function autonomousInit(), not used.
     public void autonomousInit() {
 
     }
 
+   //Creates the function autonomousPeriodic(), not used.
     public void autonomousPeriodic() {
 
     }
     
+    //Creates the function teleopInit().
     public void teleopInit() {
     	
+    	//Runs the function Dashboard_Config() in the RobotMap class.
     	RobotMap.Dashboard_Config();
+    	//Runs the function Compressor_Config() in the RobotMap class.
     	RobotMap.Compressor_Config();
+    	//Runs the function Encoder_Config() in the RobotMap class.
     	RobotMap.Encoder_Config();
+    	//Runs the function Solenoid_Config() in the RobotMap class.
     	RobotMap.Solenoid_Config();
+    	//Runs the function DriveTrain_Config() in the RobotMap class.
     	RobotMap.DriveTrain_Config();
+    	//Runs the function SpeedController_Config() in the RobotMap class.
     	RobotMap.SpeedController_Config();
     	
     }
 
+    //Creates the function teleopPeriodic()
     public void teleopPeriodic() {
     	
+    	//Runs the function gearButton() in the RobotMap class.
     	RobotMap.gearButton();
+    	//Runs the function robotDriving() in the RobotMap class.
     	RobotMap.robotDriving();
         
     }
     
+    //Creates the function testInit(), not used.
     public void testInit() {
     	
     }
 
+    //Creates the function testPeriodic(), not used.
     public void testPeriodic() {
     
     }
--- EODBOT/src/org/usfirst/frc/team5816/robot/RobotMap.java
+++ EODBOT/src/org/usfirst/frc/team5816/robot/RobotMap.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5816.robot;
 
+//Imports required classes.
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Encoder;
@@ -9,16 +10,25 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+//Creates the class RobotMap
 public class RobotMap {
 	
+	//Creates the compressor.
 	static Compressor compressor;
+	//Creates the solenoid.
 	static Solenoid solenoid;
+	//Creates the Motors.
 	static SpeedController leftMotorTop, leftMotorBack, leftMotorFront, rightMotorTop, rightMotorBack, rightMotorFront;
+	//Creates the RobotDrive types.
 	static RobotDrive drive1, drive2, drive3;
+	//Creates the encoders.
 	static Encoder encoder1, encoder2;
+	//Creates the dashboard.
 	static SmartDashboard dashboard;
+	//Creates the cameras.
 	static CameraServer camera0, camera1;
 	
+	//Creates variables for ports.
 	static int cPort = 0;
 	static int sPort = 0;
 	static int lMTPort = 0;
@@ -32,28 +42,36 @@ public class RobotMap {
 	static int e2Port1 = 2;
 	static int e2Port2 = 3;
 	
+	//Creates variables for camera names.
 	static String Camera0Port = "cam0";
 	static String Camera1Port = "cam1";
 	
+	//Creates variables for speed levels.
 	static double lowSpeed = 0.33;
 	static double mediumSpeed = 0.66;
 	static double highSpeed = 1.0;
 	static double defaultSpeed = 0.5;
 	
+	//Creates the function Compressor_Config().
 	public static void Compressor_Config() {
 		
+		//Initializes the compressor to its port.
 		compressor = new Compressor(cPort);
 		
 	}
 	
+	//Creates the function Solenoid_Config().
 	public static void Solenoid_Config() {
 		
+		//Initializes the solenoid to its port.
 		solenoid = new Solenoid(sPort);
 		
 	}
 	
+	//Creates the function SpeedController_Config().
 	public static void SpeedController_Config() {
 		
+		//Initializes the motors to their ports.
 		leftMotorTop = new VictorSP(lMTPort);
 		leftMotorBack = new VictorSP(lMBPort);
 		leftMotorFront = new VictorSP(lMFPort);
@@ -64,8 +82,10 @@ public class RobotMap {
 		
 	}
 	
+	//Creates the function DriveTrain_Config().
 	public static void DriveTrain_Config() {
 		
+		//Initializes the Drive Trains to their motors.
 		drive1 = new RobotDrive(leftMotorTop, rightMotorTop);
 		
 		drive2 = new RobotDrive(leftMotorBack, rightMotorBack);
@@ -74,43 +94,60 @@ public class RobotMap {
 		
 	}
 	
+	//Creates the function Encoder_Config().
 	public static void Encoder_Config() {
 		
+		//Initializes the encoders to their ports.
 		encoder1 = new Encoder(e1Port1, e1Port2);
 		
 		encoder2 = new Encoder(e2Port1, e2Port2);
 		
 	}
 	
+	//Suppresses a really stupid, unnecessary warning.
 	@SuppressWarnings("static-access")
+	//Creates the function Dashboard_Config().
 	public static void Dashboard_Config() {
 		
+		//Adds compressor telemetry to the dashboard.
 		dashboard.putBoolean("Compressor On: ", compressor.enabled());
+		//Adds solenoid telemetry to the dashboard.
 		dashboard.putBoolean("Solenoid Activated: ", solenoid.get());
+		//Runs the function UtilityProgramsInit() in the UtilityPrograms class.
 		UtilityPrograms.UtilityProgramsInit();
 		
 	}
 	
+	//Creates the function Camera_Config().
 	public static void Camera_Config() {
 		
+		//Gets the feed from the specified cameras.
 		camera0.startAutomaticCapture(Camera0Port);
 		camera1.startAutomaticCapture(Camera1Port);
+		//Changes the quality the cameras output.
 		camera0.setQuality(50);
 		camera1.setQuality(50);
 		
 	}
 	
+	//Creates the speedButtonConfig() class.
 	public static void speedButtonConfig() {
 		
+		//Checks to see if the lowSpeedButton is pressed.
 		if (OI.lowSpeedButton) {
+			//Sets the max speed to lowSpeed.
 			drive1.setMaxOutput(lowSpeed);
 			drive2.setMaxOutput(lowSpeed);
 			drive3.setMaxOutput(lowSpeed);
+		//Checks to see if the mediumSpeedButton is pressed.
 		} else if (OI.mediumSpeedButton){
+			//Sets the max speed to mediumSpeed.
 			drive1.setMaxOutput(mediumSpeed);
 			drive2.setMaxOutput(mediumSpeed);
 			drive3.setMaxOutput(mediumSpeed);
+		//Checks to see if the highSpeedButton is pressed.
 		} else if (OI.highSpeedButton) {
+			//Sets the max speed to highSpeed.
 			drive1.setMaxOutput(highSpeed);
 			drive2.setMaxOutput(highSpeed);
 			drive3.setMaxOutput(highSpeed);
@@ -118,19 +155,27 @@ public class RobotMap {
 		
 	}
 	
+	//Creates the function gearButton().
 	public static void gearButton() {
 		
+		//Checks to see if the highGearButton is pressed.
 		if (OI.highGearButton) {
+			//Extends the actuator.
 			solenoid.set(true);
+		//Checks to see if the lowGearButton is pressed.
 		} else if (OI.lowGearButton) {
+			//Retracts the actuator.
 			solenoid.set(false);
 		}
 		
 	}
 	
+	//Creates the function robotDriving().
 	public static void robotDriving() {
 		
+		//Starts the compressor.
 		compressor.start();
+		//Sets the drive trains to all have the same inputs.
 		drive1.tankDrive(OI.leftJoystick, OI.rightJoystick);
 		drive2.tankDrive(OI.leftJoystick, OI.rightJoystick);
 		drive3.tankDrive(OI.leftJoystick, OI.rightJoystick);
