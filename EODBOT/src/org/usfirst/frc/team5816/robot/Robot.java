63b5e5dde07f277f9a4e8783315e692ff530829f
Gra-V-Robotics <comcelion@gmail.com>
2016-07-12 14:30:18 -0400

    School Commit - Adam
    Made a super basic program to just drive.

--- EODBOT/src/org/usfirst/frc/team5816/robot/Robot.java
+++ EODBOT/src/org/usfirst/frc/team5816/robot/Robot.java
@@ -1,35 +1,24 @@
 package org.usfirst.frc.team5816.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
     public void robotInit() {
-    	UtilityPrograms.UtilityProgramsButtons();
-    	RobotMap.Camera_Config();
     }
 
     public void autonomousInit() {    	
-    	AutonomousMode.autonomousInititialize();
     }
 
     public void autonomousPeriodic() {
-    	AutonomousMode.autonomousRunning();
     }
 
     public void teleopInit() {
-    	RobotMap.Dashboard_Config();
-    	RobotMap.Compressor_Config();
-    	RobotMap.Encoder_Config();
-    	RobotMap.Solenoid_Config();	
+    	RobotMap.init();
     }
 
     public void teleopPeriodic() {
-    	RobotMap.driver();
-    	RobotMap.manipulator();
+    	RobotMap.teleop();
     }
 
     public void testInit() {
--- EODBOT/src/org/usfirst/frc/team5816/robot/RobotMap.java
+++ EODBOT/src/org/usfirst/frc/team5816/robot/RobotMap.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -15,100 +16,34 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotMap {
-
-	static Compressor compressor;
-	static Solenoid solenoid;
-	static CANTalon frontLeftMotor = new CANTalon(0);
-	static CANTalon rearLeftMotor = new CANTalon(1);
-	static CANTalon leftSlave = new CANTalon(2);
-	static CANTalon frontRightMotor = new CANTalon(3);
-	static CANTalon rearRightMotor = new CANTalon(4);
-	static CANTalon rightSlave = new CANTalon(5);
-	static CANTalon motorActuator = new CANTalon(6);
-	static RobotDrive drive = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
-	static Encoder encoder1, encoder2;
-	static SmartDashboard dashboard;
-	static CameraServer camera0, camera1;
-
-	static int cPort = 0;
-	static int sPort = 0;
-	static int e1Port1 = 0;
-	static int e1Port2 = 1;
-	static int e2Port1 = 2;
-	static int e2Port2 = 3;
-	static String Camera0Port = "cam0";
-	static String Camera1Port = "cam1";
-	static double power = 0.3;
+	static CANTalon frontLeft, frontRight, backLeft, backRight, slaveLeft, slaveRight;
+	static RobotDrive drive;
+	static int frontLeftPort = 0;
+	static int backLeftPort = 1;
+	static int slaveLeftPort = 2;
+	static int frontRightPort = 3;
+	static int backRightPort = 4;
+	static int slaveRightPort = 5;
+	static Joystick leftJoystick, rightJoystick;
 	
-	static double theoreticalDistance;
-	static double distanceConstant;
-	static double driveDistance;
-
-	public static void Compressor_Config() {
-		compressor = new Compressor(cPort);
-	}
-
-	public static void Solenoid_Config() {
-		solenoid = new Solenoid(sPort);
-	}
-	
-	public static void talonInit() {
-		leftSlave.changeControlMode(TalonControlMode.Follower);
-		rightSlave.changeControlMode(TalonControlMode.Follower);
-		leftSlave.set(0);
-		rightSlave.set(3);
-	}
-
-	public static void Encoder_Config() {
-		encoder1 = new Encoder(e1Port1, e1Port2, true, EncodingType.k4X);
-		encoder2 = new Encoder(e2Port1, e2Port2, true, EncodingType.k4X);
-	}
-
-	public static void Dashboard_Config() {
-		//Figure out encoder count & Distance
-		dashboard.putBoolean("Compressor On: ", compressor.enabled());
-		dashboard.putBoolean("Solenoid Activated: ", solenoid.get());
-		dashboard.putNumber("Left Encoder Value: ", encoder1.get());
-		dashboard.putNumber("Right Encoder Value: ", encoder2.get());
-		dashboard.putNumber("Drive To Distance: ", driveDistance);
-		UtilityPrograms.UtilityProgramsInit();
-	}
-
-	public static void Camera_Config() {
-		camera0.startAutomaticCapture(Camera0Port);
-		camera1.startAutomaticCapture(Camera1Port);
-		camera0.setQuality(50);
-		camera1.setQuality(50);
-	}
-
-	public static void driver() {
-		compressor.start();
-		drive.tankDrive(OI.leftJoystick, OI.rightJoystick);
-	}
-
-	public static void manipulator() {
-		if (OI.highGearButton) {
-			solenoid.set(true);
-		} else if (OI.lowGearButton) {
-			solenoid.set(false);
-		}
+	public static void init() {
+		frontLeft = new CANTalon(frontLeftPort);
+		backLeft = new CANTalon(backLeftPort);
+		slaveLeft = new CANTalon(slaveLeftPort);
 		
-		if (OI.XboxController.getRawButton(5)) {
-			motorActuator.set(power);
-		} else if (OI.XboxController.getRawButton(6)) {
-			motorActuator.set(-power);
-		} else {
-			motorActuator.set(0);
-		}
+		frontRight = new CANTalon(frontRightPort);
+		backRight = new CANTalon(backRightPort);
+		slaveRight = new CANTalon(slaveRightPort);
+		
+		drive = new RobotDrive(frontLeft, frontRight, backLeft, backRight);
+		slaveLeft.changeControlMode(TalonControlMode.Follower);
+		slaveRight.changeControlMode(TalonControlMode.Follower);
+		slaveLeft.set(frontLeftPort);
+		slaveRight.set(frontRightPort);
 	}
 	
-	public static void theoreticalDriveToDistanceMath() {
-		theoreticalDistance = encoder1.get() / distanceConstant;
+	public static void teleop() {
+		drive.tankDrive(leftJoystick, rightJoystick);
 	}
 	
-	public static void theoreticalDriveToDistance(double d) {
-		double driveVariable = 0;
-		driveDistance = d;
-		driveDistance *= driveVariable;
-	}
 }
